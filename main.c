fbfdb95aa17784e3d1d40eb3619b6f10267ba819
HP <poulsen.heini1@gmail.com>
2023-11-20 16:21:31 +0100

    har set mere verdier ind

--- main.c
+++ main.c
@@ -16,53 +16,133 @@ void temp (int instr){
     int rd;
     int imm;
     int funct3;
+    int rs1;
+    int rs2;
+    int funct7;
     switch (opcode) {
         case 0000011: //FMT = I
             printf("1");
             rd = (instr >> 7) & 0x01f;
-            int funct = (instr >> 12) & 0x07;
-            int rs1 = (instr >> 15) & 0x01f;
-            int imm = (instr >> 20);
-            switch (funct) {
-
+            funct3 = (instr >> 12) & 0x07;
+            rs1 = (instr >> 15) & 0x01f;
+            imm = (instr >> 20);
+            switch (funct3) {
+                case 000:
+                    break;
+                case 001:
+                    break;
+                case 010:
+                    break;
+                case 100:
+                    break;
+                case 101:
+                    break;
             }
             break;
+
+
+
+
             /*case 0001111: fence og fence.i skal ikke bruges
                 printf("2");
                 break;*/
         case 0010011: //FMT = I
             rd = (instr >> 7) & 0x01f;
+            funct3 = (instr >> 12) & 0x07;
+            rs1 = (instr >> 15) & 0x01f;
+            imm = (instr >> 20);
+            switch (funct3) {
+
+            }
             printf("3");
             break;
+
+
+
+
         case 0010111: //FMT = U
             rd = (instr >> 7) & 0x01f;
             imm = (instr >> 12);
             printf("4");
             break;
+
+
+
+
         case 0100011: //FMT = S NOTDONE
             funct3 ;
             printf("5");
             break;
+
+
+
+
         case 0110011: //FMT = R
             printf("6");
+            rd = (instr >> 7) & 0x01f;
+            funct3 = (instr >> 12) & 0x07;
+            rs1 = (instr >> 15) & 0x01f;
+            rs2 = (instr >> 20) & 0x01f;
+            funct7 = (instr >> 25);
+            switch (funct3) {
+
+            }
             break;
+
+
+
+
         case 0110111: //FMT = U
             rd = (instr >> 7) & 0x01f;
             imm = (instr >> 12);
             printf("7");
             break;
+
+
+
+
         case 1100011: //FMT = SB
             printf("8");
             break;
+
+
+
+
         case 1100111: //FMT = I
             printf("9");
+            rd = (instr >> 7) & 0x01f;
+            funct3 = (instr >> 12) & 0x07;
+            rs1 = (instr >> 15) & 0x01f;
+            imm = (instr >> 20);
+            switch (funct3) {
+
+            }
             break;
+
+
+
+
         case 1101111: //FMT = UJ
             printf("10");
             break;
+
+
+
+
         case 1110011://FMT = I  det er kun ecall som skal bruges
             printf("11");
+            rd = (instr >> 7) & 0x01f;
+            funct3 = (instr >> 12) & 0x07;
+            rs1 = (instr >> 15) & 0x01f;
+            imm = (instr >> 20);
+            switch (funct3) {
+
+            }
             break;
+
+
+
+
         default:
             printf("Opcode %s not yet implemented", opcode);
     }
